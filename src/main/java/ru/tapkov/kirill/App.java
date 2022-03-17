1db5315cd7465bbb67f08504c45fcd5e3fb842f4
Kirill Tapkov <izhjup@mail.ru>
2022-03-18 01:03:35 +0400

    First commit

---
+++ src/main/java/ru/tapkov/kirill/App.java
@@ -0,0 +1,14 @@
+package ru.tapkov.kirill;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        org.apache.commons.lang3.text.WordUtils.capitalize("AC");
+    }
+}
